2af68c17bb3021fe6c20a6d68e76ae8955fd2cdc
Abootic <102881124+Abootic@users.noreply.github.com>
2023-08-31 00:25:56 +0300

    commit

---
+++ ecomerce/Application/Interfaces/Repositories/IMainClassificationRepository.cs
@@ -0,0 +1,14 @@
+﻿using EcommereceWeb.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Target.Application.Interfaces.Common;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IMainClassificationRepository : IGenericRepository<MainClassification>
+    {
+    }
+}
