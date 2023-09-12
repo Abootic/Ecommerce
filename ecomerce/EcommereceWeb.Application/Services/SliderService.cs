69b719423d209bff1d90c032f00256bf7997bab7
Abootic <102881124+Abootic@users.noreply.github.com>
2023-09-12 21:56:32 +0300

    commit

--- ecomerce/EcommereceWeb.Application/Services/SliderService.cs
+++ ecomerce/EcommereceWeb.Application/Services/SliderService.cs
@@ -1,6 +1,9 @@
-﻿using EcommereceWeb.Application.DTOs;
+﻿using AutoMapper;
+using EcommereceWeb.Application.DTOs;
 using EcommereceWeb.Application.Interfaces.Common;
 using EcommereceWeb.Application.Interfaces.Services;
+using EcommereceWeb.Application.Wrapper;
+using EcommereceWeb.Domain.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +15,35 @@ namespace EcommereceWeb.Application.Services
 {
     public class SliderService : ISliderService
     {
-        public Task<IResult<SliderDto>> Add(SliderDto entity, CancellationToken cancellationToken = default)
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly IMapper _mapper;
+
+        public SliderService(IRepositoryManager repositoryManager, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _repositoryManager = repositoryManager;
+            _mapper = mapper;
+        }
+
+        public async Task<IResult<SliderDto>> Add(SliderDto entity, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+
+                var entityMap = _mapper.Map<Slider>(entity);
+                var res = await _repositoryManager.SliderRepository.AddAndReturn(entityMap);
+                if (res != null)
+                {
+                    await _repositoryManager.UnitOfWork.CompleteAsync();
+                    var map = _mapper.Map<SliderDto>(res);
+                    return await Result<SliderDto>.SucessAsync(map, "تم الاضافة بنجاح");
+                }
+                return await Result<SliderDto>.FailAsync($"لم يتم الاضافة ");
+
+            }
+            catch (Exception ex)
+            {
+                return await Result<SliderDto>.FailAsync($"something error {ex.Message} ");
+            }
         }
 
         public Task<IResult<IEnumerable<SliderDto>>> Find(Expression<Func<SliderDto, bool>> expression, CancellationToken cancellationToken = default)
@@ -22,24 +51,86 @@ namespace EcommereceWeb.Application.Services
             throw new NotImplementedException();
         }
 
-        public Task<IResult<IEnumerable<SliderDto>>> GetAll(CancellationToken cancellationToken = default)
+        public async Task<IResult<IEnumerable<SliderDto>>> GetAll(CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var res = await _repositoryManager.SliderRepository.GetAll();
+                if (res != null)
+                {
+                    return await Result<IEnumerable<SliderDto>>.SucessAsync(_mapper.Map<IEnumerable<SliderDto>>(res));
+                }
+                return await Result<IEnumerable<SliderDto>>.FailAsync($"لايوجد بيانات ");
+
+            }
+            catch (Exception ex)
+            {
+                return await Result<IEnumerable<SliderDto>>.FailAsync($"something error {ex.Message} ");
+            }
         }
 
-        public Task<IResult<SliderDto>> GetById(int Id, CancellationToken cancellationToken = default)
+        public async Task<IResult<SliderDto>> GetById(int Id, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var res = await _repositoryManager.SliderRepository.GetById(Id);
+                if (res != null)
+                {
+                    return await Result<SliderDto>.SucessAsync(_mapper.Map<SliderDto>(res));
+                }
+                return await Result<SliderDto>.FailAsync(" لايوجد بيانات لهذا الرقم");
+
+            }
+            catch (Exception ex)
+            {
+                return await Result<SliderDto>.FailAsync($"something error {ex.Message} ");
+            }
         }
 
-        public Task<IResult<SliderDto>> Remove(int Id, CancellationToken cancellationToken = default)
+        public async Task<IResult<SliderDto>> Remove(int Id, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var entity = await _repositoryManager.SliderRepository.GetById(Id);
+                if (entity != null)
+                {
+                    var res = await _repositoryManager.SliderRepository.Remove(entity);
+                    await _repositoryManager.UnitOfWork.CompleteAsync();
+                    if (res != null)
+                    {
+                        return await Result<SliderDto>.SucessAsync(_mapper.Map<SliderDto>(res));
+                    }
+                    return await Result<SliderDto>.FailAsync(" لم يتم حذف البيانات");
+                }
+                return await Result<SliderDto>.FailAsync(" لايوجد بيانات لهذا الرقم");
+
+            }
+            catch (Exception ex)
+            {
+                return await Result<SliderDto>.FailAsync($"something error {ex.Message} ");
+            }
         }
 
-        public Task<IResult<SliderDto>> Update(SliderDto entity, CancellationToken cancellationToken = default)
+        public async Task<IResult<SliderDto>> Update(SliderDto entity, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var entityMap = _mapper.Map<Slider>(entity);
+                var res = await _repositoryManager.SliderRepository.Update(entityMap);
+                if (res != null)
+                {
+                    await _repositoryManager.UnitOfWork.CompleteAsync();
+                    var map = _mapper.Map<SliderDto>(res);
+                    return await Result<SliderDto>.SucessAsync(map, "تم التعديل بنجاح");
+                }
+                return await Result<SliderDto>.FailAsync($"لم يتم التعديل ");
+
+            }
+            catch (Exception ex)
+            {
+                return await Result<SliderDto>.FailAsync($"something error {ex.Message} ");
+            }
         }
+
     }
 }
--- ecomerce/EcommereceWeb.MVC/Controllers/SliderController.cs
+++ ecomerce/EcommereceWeb.MVC/Controllers/SliderController.cs
@@ -1,13 +1,79 @@
-﻿using EcommereceWeb.MVC.Controllers.Base;
+﻿using EcommereceWeb.Application.DTOs;
+using EcommereceWeb.MVC.Controllers.Base;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EcommereceWeb.MVC.Controllers
 {
     public class SliderController : BaseMVCController
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var res = await ServiceManager.SliderService.GetAll();
+            if (res.Status.Succeeded)
+            {
+                return View(res.Data);
+            }
+            return View();
+
+        }
+        public async Task<IActionResult> Create()
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SliderDto entity)
+        {
+
+            if (entity == null) { TempData["err"] = "null value"; return View(entity); }
+            var res = await ServiceManager.SliderService.Add(entity);
+            if (res.Status.Succeeded)
+            {
+                TempData["suc"] = res.Status.message;
+                return RedirectToAction("Index");
+            }
+            TempData["err"] = res.Status.message;
+            return View(entity);
+        }
+
+        public async Task<IActionResult> Edit(int Id)
+        {
+            var res = await ServiceManager.SliderService.GetById(Id);
+            if (res.Status.Succeeded)
+            {
+                return View(res.Data);
+            }
+            TempData["err"] = res.Status.message;
+
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(SliderDto entity)
+        {
+
+            if (entity == null) { TempData["err"] = "null value"; return View(entity); }
+            var res = await ServiceManager.SliderService.Update(entity);
+            if (res.Status.Succeeded)
+            {
+                TempData["suc"] = res.Status.message;
+                return RedirectToAction("Index");
+            }
+            TempData["err"] = res.Status.message;
+            return View(entity);
+        }
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var res = await ServiceManager.SliderService.Remove(Id);
+            if (res.Status.Succeeded)
+            {
+                TempData["suc"] = res.Status.message;
+                return RedirectToAction("Index");
+            }
+            TempData["err"] = res.Status.message;
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
