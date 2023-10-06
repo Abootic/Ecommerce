8e04e541400edf1782bb1cf269511ebe9d207456
Abootic <102881124+Abootic@users.noreply.github.com>
2023-10-07 01:25:09 +0300

    commit

--- ecomerce/EcommereceWeb.MVC/Controllers/ProductController.cs
+++ ecomerce/EcommereceWeb.MVC/Controllers/ProductController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcommereceWeb.Application.DTOs;
+using EcommereceWeb.Application.Services;
+using EcommereceWeb.MVC.Controllers.Base;
+using EcommereceWeb.MVC.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcommereceWeb.MVC.Controllers
 {
-    public class ProductController : Controller
+    public class ProductController : BaseMVCController
     {
         public IActionResult Index()
         {
+              ProductAdditionalVM vm = new ProductAdditionalVM();
+            vm.checkBoxListVms = new List<CheckBoxListVm>();
+            vm.CheckBoxListVm = new CheckBoxListVm();
+            vm.productDto = new ProductDto();
+            return View(vm); vm = new ProductAdditionalVM();
+            vm.checkBoxListVms = new List<CheckBoxListVm>();
+            vm.CheckBoxListVm = new CheckBoxListVm();
+            return View(vm);
+        
+        }  public IActionResult Create()
+        {
             return View();
+        }
+        public async Task<IActionResult> getAttrItems(int id)
+        {
+            var res = await ServiceManager.AttributeItemService.Find(a => a.AttributeId == id);
+
+            if (res.Status.Succeeded)
+            {
+                ProductAdditionalVM vm = new ProductAdditionalVM();
+                vm.checkBoxListVms = new List<CheckBoxListVm>();
+                foreach (var item in res.Data)
+                {
+
+                    vm.CheckBoxListVm = new CheckBoxListVm
+                    {
+                        id = item.Id,
+                        name = item.ArName
+                    };
+
+                    vm.checkBoxListVms.Add(vm.CheckBoxListVm);
+                }
+
+                return PartialView("_checkBoxList", vm);
+            }
+            return BadRequest("noo data");
+
         }
     }
 }
--- ecomerce/EcommereceWeb.MVC/ViewModel/ProductAdditionalVM.cs
+++ ecomerce/EcommereceWeb.MVC/ViewModel/ProductAdditionalVM.cs
@@ -7,6 +7,7 @@ namespace EcommereceWeb.MVC.ViewModel
         public List<CheckBoxListVm> checkBoxListVms { get; set; }
         public ProductAttributeDto ProductAttributeDto { get; set; }
         public CheckBoxListVm CheckBoxListVm { get; set; }
+        public ProductDto productDto { get; set; }
         public int ProductId { get; set; }
         public int? AttributeId { get; set; }
         public string AttributeItemId { get; set; }
